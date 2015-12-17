80ee88861c5ca77f54917d92eeb8419b97d040a2
adeng <adeng@media.ucla.edu>
2015-12-16 21:02:48 -0800

    Added segment

--- stocks/www/app/page3/page3.ts
+++ stocks/www/app/page3/page3.ts
@@ -1,11 +1,15 @@
 import {Page} from 'ionic-framework/ionic';
 
-
 @Page({
   templateUrl: 'app/page3/page3.html'
 })
 export class Page3 {
+  display: string = "have";
   constructor() {
 
+  }
+  
+  fillList(display: string) {
+    console.log(display);
   }
 }
