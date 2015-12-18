17157916d37eaffead6c727c5e8427a88241e348
adeng <adeng@media.ucla.edu>
2015-12-18 11:31:46 -0800

    Colored tabs and promise progress

--- stocks/www/app/stock-info/stock-info.ts
+++ stocks/www/app/stock-info/stock-info.ts
@@ -1,14 +1,25 @@
 import {Page, NavParams} from 'ionic-framework/ionic';
 import {Stock} from '../libs/stocks/stock';
+import {Http} from 'angular2/http';
 
 @Page({
   templateUrl: 'app/stock-info/stock-info.html',
 })
 
 export class StockInfo {
+  data: Object;
   stock: Stock;
-  constructor(navParams: NavParams) {
-    this.stock = new Stock(navParams.get('stock'));
-    // this.stock = navParams.get('stock');
+  
+  name: Promise<string>;
+  
+  constructor(navParams: NavParams, http: Http) {
+    this.stock = new Stock(navParams.get('stock'), http);
+    this.stock.getStockData(navParams.get('stock'), http).then(function(val) {
+      this.data = val;
+      
+      this.name = new Promise<string>(function(resolve) {
+        resolve(this.data.Name);
+      });
+    });
   }
 }
