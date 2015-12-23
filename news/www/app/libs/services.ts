cb68985ac12cf0ec2952883aba0e60c0f1a744bd
Albert Deng <adeng@media.ucla.edu>
2015-12-22 18:55:01 -0800

    Copying from UCLA Daily Bruin

---
+++ news/www/app/libs/services.ts
@@ -0,0 +1,90 @@
+import {Http} from 'angular2/http';
+
+export class XMLParser {
+    parser;
+    
+    constructor() {
+        this.parser = new DOMParser();
+    }
+    
+    getTags(dom: Element, tag: string) {
+        return [].slice.call(dom.getElementsByTagName(tag));
+    }
+    
+    getRSS(http: Http, src: string, category: string) {
+        let a = this;
+        return new Promise<any>(function(resolve) {
+            http.get(src).subscribe(
+                data => {
+                    let arr = a.processRSS(data.text(), category);
+                    resolve(arr);
+                },
+                err => {
+					http.get('/app/libs/data/sample-rss.xml').subscribe(
+						res => {
+                            let arr = a.processRSS(res.text(), category);
+                            resolve(arr);
+						}
+					);
+				}
+            );
+        });
+    }
+    
+    parseRSS(xml: string, tag: string) {
+        let doc = this.parser.parseFromString(xml, 'application/xml');
+        return [].slice.call(doc.getElementsByTagName(tag));
+    }
+    
+    /**
+     * Finds all image and video links given a HTML string.
+     * 
+     * @author: Richard Sun
+     */
+    processNode(html: string) {
+        var doc = this.parser.parseFromString(html, 'text/html'); 
+		var images = this.getTags(doc, 'img'); 
+ 		var videos = this.getTags(doc, 'video').concat(this.getTags(doc, 'iframe')); 
+        
+ 		return { 
+ 			images: images.map(function(img) { 
+ 				return img.getAttribute('src'); 
+ 			}), 
+ 			videos: videos.map(function(vid) { 
+ 				return vid.getAttribute('src'); 
+ 			}) 
+ 		}; 
+
+    }
+    
+    processRSS(xml: string, category: string) {
+        let posts = this.parseRSS(xml, 'item');        
+        let arr = new Array<Object>();
+        
+        for(let i = 0; i < posts.length; i++) {
+            let obj = new Object();
+            for(let j = 0; j < posts[i].children.length; j++) {
+                let content = posts[i].children[j].innerHTML;
+                switch(posts[i].children[j].nodeName.toLowerCase()) {
+                    case "title":
+                        obj['title'] = content;
+                        break;
+                    case "link":
+                        obj['link'] = content;
+                        break;
+                    case "pubdate":
+                        obj['date'] = Date.parse(content);
+                        break;
+                    case "content:encoded":
+                        obj['media'] = this.processNode(content.substring(9, content.length - 3));
+                        obj['htmlcontent'] = content.substring(9, content.length - 3);
+                        break;
+                }
+            }
+            obj['category'] = category;
+            arr.push(obj);
+        }
+        
+        return arr;
+    }
+}
