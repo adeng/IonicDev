015c1a3f9a9186cf22c82218cf0cbfa3b2d6eb48
Albert Deng <adeng@media.ucla.edu>
2015-12-22 19:45:03 -0800

    Post list now slides!

--- ucla-daily-bruin/www/app/postlist/postlist.ts
+++ ucla-daily-bruin/www/app/postlist/postlist.ts
@@ -10,14 +10,19 @@ export class PostList {
     objs: Array<Object>;
     
     // this should be passed in as an param or something in the future
-    src: Object = {name: "News", url: "http://dailybruin.com/category/news/feed/"};
+    categories: Array<Object> = [{name: "News", url: "http://dailybruin.com/category/news/feed/"}, {name: "Sports", url: "http://dailybruin.com/category/sports/feed/"}, {name: "A&E", url: "http://dailybruin.com/category/arts-entertainment/feed/"}, {name: "Opinion", url: "http://dailybruin.com/category/opinion/feed/"}, {name: "Radio", url: "http://dailybruin.com/category/radio/feed/"}, {name: "Video", url: "http://dailybruin.com/category/video/feed/"}];
     
     constructor(http: Http) {
         this.parser = new XMLParser();
-        this.objs = new Array<Object>();
-        this.parser.getRSS(http, this.src.url, this.src.name).then( data => {
-            this.objs = data;
-            console.log(data);
-        });
+        this.objs = new Array<Array<Object>>();
+        
+        let nameArray = new Array();
+        for(let i = 0; i < this.categories.length; i++) {
+            this.objs[i] = new Array<Object>();
+            nameArray.push(this.categories[i].name);
+            this.parser.getRSS(http, this.categories[i].url, this.categories[i].name).then( data => {
+                this.objs[nameArray.indexOf(data[0].category)] = data;
+            });
+        }
     }
 }
